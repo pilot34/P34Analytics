d52b83f02bf04593cbdaf13cc16b94d32040c0f0
gtarasov <gleb34@gmail.com>
2013-04-23 13:59:20 +0400

    P34Analytics

---
+++ P34Analytics.m
@@ -0,0 +1,58 @@
+//
+//  Analytics.m
+//  Intuit
+//
+//  Created by Глеб Тарасов on 25.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "P34Analytics.h"
+#import "GAI.h"
+#import "GAITracker.h"
+#import "UIDevice+IdentifierAddition.h"
+#import "Flurry.h"
+
+static BOOL __use_flurry;
+
+@implementation P34Analytics
+
++ (void)startWithGoogleId:(NSString *)google flurryId:(NSString *)flurryId
+{
+    [self startWithId:google];
+    [Flurry startSession:flurryId];
+    __use_flurry = YES;
+}
+
++ (void)startWithId:(NSString *)id
+{
+    [GAI sharedInstance].trackUncaughtExceptions = YES;
+    [[GAI sharedInstance] trackerWithTrackingId:id];
+}
+
++ (void)trackPageView:(NSString *)page
+{
+    [[[GAI sharedInstance] defaultTracker] trackView:page];
+    
+    if (__use_flurry)
+        [Flurry logPageView];
+}
+
++ (void)trackEvent:(NSString *)event
+            action:(NSString *)action
+{
+    [[[GAI sharedInstance] defaultTracker] trackEventWithCategory:event
+                                                       withAction:action
+                                                        withLabel:action
+                                                        withValue:@0];
+    
+    if (__use_flurry)
+        [Flurry logEvent:event withParameters:@{ @"action" : action }];
+}
+
++ (void)trackEvent:(NSString *)event
+{
+    [self trackEvent:event action:event];
+}
+
+
+@end
---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios, '5.0'
+pod 'UIDeviceAddition'
+pod 'GoogleAnalytics-iOS-SDK'
+pod 'FlurrySDK'
